2048a81a2b88d8ae22a19272e44ccea7ee46618b
Mahjabin Islam Sinthiha <mahzabin.islam26@gmail.com>
2026-05-03 21:44:37 +0600

    code from 207

--- src/Token.java
+++ src/Token.java
@@ -1,17 +1,32 @@
 public class Token {
+
     public enum Type {
-        IDENTIFIER, NUMBER, ASSIGN, PLUS, MULTIPLY, EOF
+        IDENTIFIER,
+        NUMBER,
+        ASSIGN,
+        PLUS,
+        MULTIPLY,
+        EOF
     }
 
     public final Type type;
     public final String value;
 
+    // Constructor
     public Token(Type type, String value) {
         this.type = type;
         this.value = value;
     }
 
+    // Convert token to string representation
+    @Override
     public String toString() {
-        return "Token(" + type + ", " + value + ")";
+        StringBuilder result = new StringBuilder();
+        result.append("Token(")
+              .append(type)
+              .append(", ")
+              .append(value)
+              .append(")");
+        return result.toString();
     }
 }
